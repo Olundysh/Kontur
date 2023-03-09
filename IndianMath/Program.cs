5dfa60880b4dd4258f4a5b315ec73d49c3d42593
olundysh <olgavecholga@gmail.com>
2023-03-09 10:40:57 +0300

    IndianMath and StdInOut initialized.

---
+++ IndianMath/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace IndianMath;
+
+public static class Program
+{
+    public static void Main ()
+    {
+        string input = Console.ReadLine()!;
+        var max = long.Parse(new string(input.OrderByDescending(c => c).ToArray()));
+        var min = long.Parse(new string(input.OrderBy(c => c).ToArray()));
+        Console.WriteLine(max - min);
+    }
+}
---
+++ StdInOut/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StdInOut;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var counter = 0;
+        while (counter != 3)
+        {
+            // Console.WriteLine("Print your text");
+            string? ourData = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(ourData))
+            {
+                counter++;
+            }
+            else
+            {
+                counter = 0;
+                Console.WriteLine(ourData.Length);                
+            }
+        }
+    }
+}
