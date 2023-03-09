bad4140b617d66fa9db9320112ae4bbd7fea3263
olundysh <olgavecholga@gmail.com>
2023-03-09 11:42:47 +0300

    MissingNumber project ready.

---
+++ MissingNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MissingNumber;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var arrLength = int.Parse(Console.ReadLine());
+        var negativeNums = 0;
+        var positiveNums = 0;
+        for (var i = 0; i < arrLength; i++)
+        {
+            var number = int.Parse(Console.ReadLine());
+            if (number < 0) negativeNums += number;
+            if (number > 0) positiveNums += number;
+        }
+        Console.WriteLine(-(negativeNums + positiveNums));
+    }
+}
+                                              
